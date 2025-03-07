845f4471ab16212533ac471dba659945229b3ace
Faizan. A Rakin <rakinfaizanahmad@gmail.com>
2025-03-07 14:44:59 -0800

    Add greeting and use again function. Add Commenting

--- main.py
+++ main.py
@@ -2,23 +2,34 @@
 
 # Chosen cipher : Ceaser Cipher 
 
-# Either call a function to encrypt or to decrypt the string.
+# Function to greet the user
+def greeting() -> str :
+    """
+    This function asks the user's name and greets the user
+    """
+    name = input("What is your name? \n").title().strip()
 
-# Create two functions that will accept parameters in the form of a list or a dictionary that will contain the string meant to be encrypted or decrypted. 
-def encrypter(message: list, shift_value: int) -> str:
+    print(f"Hi {name}! Welcome to my Number Guessing Game.") 
+
+
+# Function that will accept parameters in the form of a list or a dictionary that will contain the string meant to be encrypted
+def encrypter(possible_inputs: list, message: list, shift_value: int) -> str:
     """
     This function encrypts the message into a secret code 
     """
-    possible_inputs = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
-                  "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", " ", "`", "~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+"
-                  "[", "]", "|", "{", "}", "'", ";", ":", ",", ".", "/", "<", ">", "?"]
-    
+    # Store each new letter in an empty list
     encoded_list = []
 
+    # Loop through all the letters in the inputted message
     for old_letter in message:
+        
 
         if old_letter in possible_inputs:
+
+            # If the letter is in the list of possible inputs, find its index
             old_index = possible_inputs.index(old_letter)
+
+            # Add shift value to the index
             new_index = (old_index + shift_value)  % len(possible_inputs) # returns the remainder using "%" modulus operator which loops back to the start of the list
             encoded_list.append(possible_inputs[new_index])
             
@@ -30,15 +41,12 @@ def encrypter(message: list, shift_value: int) -> str:
 
     print(encrypted_message)
 
-
-def decrypter(secret_message: list, shift_value: int) -> str:
+# Function that will accept parameters in the form of a list or a dictionary that will contain the string meant to be decrypted
+def decrypter(possible_inputs: list, secret_message: list, shift_value: int) -> str:
     """
     This function decrypts the secret code into a message
     """
-    possible_inputs = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
-                "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", " ", "`", "~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+"
-                "[", "]", "|", "{", "}", "'", ";", ":", ",", ".", "/", "<", ">", "?"]
-    
+
     decoded_list = []
 
     for letter in secret_message:
@@ -55,36 +63,34 @@ def decrypter(secret_message: list, shift_value: int) -> str:
     print(decrypted_message)
 
 
-# One function will encrypt while the other will decrypt using the cipher technique you have chosen. 
-
-# This should involve both loops and if statements.
-
-# Present the final encrypted or decrypted string to the user. 
-
-# The user must be able to copy that output and run it through the program again to perfectly encrypt or decrypt it multiple times without any mistakes
-
-# Create a program that will start by asking the user whether they want to decrypt or encrypt a message.
 def option():
     """
     This function gives the user the option to encrypt or decrypt a message and then asks for the message. 
     """
+    # List of all possible inputs from the user
+    possible_inputs = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
+                "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", " ", "`", "~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+"
+                "[", "]", "|", "{", "}", "'", ";", ":", ",", ".", "/", "<", ">", "?"]
+
     while True:
         try:
+            # Ask if the user wants to encrypt or decrypt 
             choice = input("Please choose to encrypt or decrypt a message [e/d] : ").strip().lower()
 
             # Accept a string that the program will either encrypt or decrypt.
             message = input("\nWhat is your secret message? [No quotation marks!]: ").strip().lower()
 
+            # Ask for a shift value for Caeser Cipher
             shift_value = int(input("Please enter a shift value [Same shift value needed to decrypt messages!]: "))
             
             # Call the encrypter function to encrypt the message
             if choice == "e":
-                encrypter(message, shift_value)
+                encrypter(possible_inputs, message, shift_value)
                 continue
 
-            # Call th decrypter function to decrypt the message
+            # Call the decrypter function to decrypt the message
             elif choice == "d":
-                decrypter(message, shift_value)
+                decrypter(possible_inputs, message, shift_value)
                 break
 
             # Continue the loop if a valid choice is not entered
@@ -92,10 +98,35 @@ def option():
                 print("Please enter [e/d] ")
                 continue
             
-        
+        # Continue the loop  if invalid arguements are inputted
         except ValueError:
             print("Please input a valid arguements! ")
             continue
 
+# Function to let the user use the Enigma Machine till they exit
+def use_again() -> str:
+    """
+    This function gives the user the choice to use the Enigma Machine again
+    """
+    while True:
+
+        # Ask if they want to use again, only accepting 'y' or 'n'
+        choice = input("Do you wish to use the Enima Machine again? [y/n] - ").strip().lower()
+
+        # Run option function again if user chooses "y"
+        if choice == "y":
+            option()
+
+        # End the program if user chooses "n"    
+        elif choice == "n":
+            break
+
+        # Tell user to choose only between "y/n"
+        else:
+            print("Please enter y/n ") 
+
+
 if __name__ == "__main__":
+    greeting()
     option()
+    use_again()
